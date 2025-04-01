c3897bb8215243369ab213a47173f1e1db6152c7
Edgar Maurel <edgar.maurel@epitech.eu>
2025-04-01 16:37:57 +0200

    feat - loading bmp

--- main.py
+++ main.py
@@ -1,16 +1,54 @@
 from tercen.client import context as ctx
 import numpy as np
+import pandas as pd
+import json
+import os
+from PIL import Image
 
-tercenCtx = ctx.TercenContext()
-
-df = (
-    tercenCtx
-    .select(['.y', '.ci', '.ri'], df_lib="pandas")
-    .groupby(['.ci','.ri'], as_index=False)
-    .mean()
-    .rename(columns={".y":"mean"})
-    .astype({".ci": np.int32, ".ri": np.int32})
+tercenCtx = ctx.TercenContext(
+    workflowId="241160841cead65995c0c45d0a011ce8",
+    stepId="43f1bc61-3638-41e0-8b05-f5a2c888ea38",
+    username="admin", # if using the local Tercen instance
+    password="admin", # if using the local Tercen instance
+    serviceUri = "http://127.0.0.1:5402/" # if using the local Tercen instance 
 )
+
+imageDocumentId = tercenCtx.cubeQuery.toJson()["relation"]["relation"]["inMemoryTable"]["columns"][0]["values"][0]
+print(imageDocumentId)
+
+aliasId = tercenCtx.document_alias_to_id(imageDocumentId, "documentId")
+
+
+httpResponse = tercenCtx.client.fileService.download(aliasId)
+
+with open("image.bmp", "wb") as f:
+    f.write(httpResponse.read())
+
+def bmp_to_dataset(image_path):
+    print("Loading image from: ", image_path)
+    # Open the BMP image
+    img = Image.open(image_path)
+    img = img.convert('RGB')  # Ensure RGB format
+    
+    width, height = img.size
+
+    #res is an array of 3 columns [color_code, x, y] and width*height rows
+    res = np.zeros((width*height, 3), dtype=int)
+
+    for y in range(height):
+            for x in range(width):
+                r, g, b = img.getpixel((x, y))
+                color_code = (r << 16) + (g << 8) + b
+                res[y*width + x] = [color_code, x, y]
+
+    return res
+
+df = bmp_to_dataset("image.bmp")
+
+#delete the image file
+os.remove("image.bmp")
+
+df = pd.DataFrame(df, columns=["color_code", "x", "y"])
 
 df = tercenCtx.add_namespace(df) 
 tercenCtx.save(df)
